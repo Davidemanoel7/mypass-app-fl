5bdd8780f6da54eb074fc05a27e8698201aaf967
Davidemanoel7 <davidemanoel34@gmail.com>
2024-02-18 18:09:42 -0300

    SignInView

---
+++ lib/screens/auth/signInControll.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart';
+import 'package:get/get.dart';
+
+class SignInControl extends GetxController{
+  
+  var isAuth = false.obs;
+  var isEmailValid = false.obs;
+  var isPassValid = false.obs;
+
+  Future<bool> logIn() async {
+    // await fe
+    return true;
+  }
+}
