1f48195ab15430671df0f315696f8e8d73dd32bf
Davidemanoel7 <davidemanoel34@gmail.com>
2024-02-26 15:34:41 -0300

    Init signIn

--- lib/screens/auth/signInControll.dart
+++ lib/screens/auth/signInControll.dart
@@ -1,14 +1,76 @@
-import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mypass/services/fetchData.dart';
 
 class SignInControl extends GetxController{
   
   var isAuth = false.obs;
   var isEmailValid = false.obs;
   var isPassValid = false.obs;
+  var token = ''.obs;
 
-  Future<bool> logIn() async {
-    // await fe
+  Future<bool> logIn( String user, String senha) async {
+    
+    final dynamic body = { 'user': user, 'password': senha };
+    
+    var resp = await fetchData(
+      Requests.signIn,
+      body: body
+    );
+
+    if ( resp.statusCode == 200 ) {
+      // var auth = Auth.fromJson( jsonDecode(resp.body) as Map<String, dynamic>);
+      // debugPrint('$auth');
+      isAuth(true);
+      return true;
+      
+    } else {
+      return false;
+      // throw Exception('Failed to make request...');
+    }
+  }
+
+  Future<bool> signUp() async {
+    // const response = http.get()
     return true;
+  }
+
+  // testGet() async {
+  //   var resp = await fetchData(
+  //     'https://jsonplaceholder.typicode.com/todos/1',
+  //     Requests.getUser
+  //   );
+  //   debugPrint('\nResp => $resp\n');
+
+  //   final result = resp == null
+  //   ? null
+  //   : List<dynamic>.from(resp['']).map( (e) => debugPrint(e));
+
+  //   return result;
+  // }
+}
+
+class Auth {
+
+  final String token;
+  final String message;
+
+  Auth({
+    required this.token,
+    required this.message
+  });
+
+  factory Auth.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'token': String token,
+        'message': String message,
+      } => Auth(
+        token: token,
+        message: message
+      ),
+      _ => throw const FormatException('Failed to load json data')
+    };
   }
 }
--- lib/screens/auth/signInView.dart
+++ lib/screens/auth/signInView.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:mypass/screens/auth/signInControll.dart';
 import 'package:mypass/utils/inputValidation.dart';
 import 'package:mypass/utils/themes.dart';
-import 'package:mypass/widgets/components/BasicForm.dart';
 
 class SignInView extends StatelessWidget {
   SignInView({super.key});
@@ -18,143 +17,148 @@ class SignInView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.all(24.0),
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              './lib/assets/images/myPass.png'  
-            ),
-            Form(
-              autovalidateMode: AutovalidateMode.always,
-              child: TextFormField(
-                controller: emailEditControl,
-                style: const TextStyle(
-                  backgroundColor: Colors.white10,
+      body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.manual,
+        child: Container(
+          margin: const EdgeInsets.all(24.0),
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                child: Image.asset(
+                  './lib/assets/images/myPass.png'  
                 ),
-                decoration: InputDecoration(
-                  prefixIcon: const Icon( Icons.email_outlined ),
-                  hintText: 'Digite seu email',
-                  hintStyle: MyPassFonts.style.kLabelSmall(context, color: const Color.fromARGB(73, 0, 0, 0)),
-                  border: UnderlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0)
-                  ),
-                  fillColor: Colors.white54,
-                  filled: true,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: MyPassColors.purpleLight,
-                      width: 1.0
-                    ),
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                  focusColor: MyPassColors.purpleLight,
-                ),
-                textInputAction: TextInputAction.done,
-                validator: (value) => validInput.validationMessage( ValidationType.email, value! ),
-                onChanged: (value) {
-                  if (value.isEmail){
-                    signInControl.isEmailValid(true);
-                  }
-                },
               ),
-            ),
-
-            Form(
-              autovalidateMode: AutovalidateMode.always,
-              child: TextFormField(
-                controller: senhaEditControl,
-                style: const TextStyle(
-                  backgroundColor: Colors.white10,
-                ),
-                decoration: InputDecoration(
-                  prefixIcon: const Icon( Icons.lock_open_outlined ),
-                  hintText: 'Digite sua senha',
-                  hintStyle: MyPassFonts.style.kLabelSmall(context, color: const Color.fromARGB(73, 0, 0, 0)),
-                  border: UnderlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0)
-                  ),
-                  fillColor: Colors.white54,
-                  filled: true,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: MyPassColors.purpleLight,
-                      width: 1.0
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.always,
+                  child: TextFormField(
+                    controller: emailEditControl,
+                    style: const TextStyle(
+                      backgroundColor: Colors.white10,
                     ),
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                  focusColor: MyPassColors.purpleLight,
-                ),
-                textInputAction: TextInputAction.done,
-                validator: (value) => validInput.validationMessage( ValidationType.senha, value! ),
-                onChanged: (value) {
-                  if (RegExp(r'^[a-zA-Z0-9_!@#.$]{6,20}$').hasMatch(value)){
-                    signInControl.isPassValid(true);
-                  }
-                },
-              ),
-            ),
-
-            Obx(() => 
-              ( signInControl.isEmailValid.value && signInControl.isPassValid.value )
-              ? GestureDetector(
-                onTap: (){
-                  
-                },
-                child: const Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: CircleAvatar(
-                      backgroundColor: MyPassColors.purpleLight,
-                      radius: 24,
-                      child: Icon(
-                        Icons.arrow_forward,
-                        color: MyPassColors.whiteF0,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon( Icons.email_outlined ),
+                      hintText: 'Digite seu email',
+                      hintStyle: MyPassFonts.style.kLabelSmall(context, color: const Color.fromARGB(73, 0, 0, 0)),
+                      border: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0)
                       ),
-                    ),
-                  ), 
-                ),
-              )
-            : GestureDetector(
-                onTap: (){
-                  
-                },
-                child: const Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: CircleAvatar(
-                      backgroundColor: MyPassColors.greyBD,
-                      radius: 24,
-                      child: Icon(
-                        Icons.arrow_forward,
-                        color: MyPassColors.black1B,
+                      fillColor: Colors.white54,
+                      filled: true,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: MyPassColors.purpleLight,
+                          width: 1.0
+                        ),
+                        borderRadius: BorderRadius.circular(16.0),
                       ),
+                      focusColor: MyPassColors.purpleLight,
                     ),
-                  ), 
-                ),
-              )
-            ),
-            TextButton(
-              style: ButtonStyle(
-                overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                  (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.focused))
-                      return Colors.red;
-                    return null; // Defer to the widget's default.
-                  }
+                    textInputAction: TextInputAction.done,
+                    validator: (value) => validInput.validationMessage( ValidationType.email, value! ),
+                    onChanged: (value) {
+                      if (value.isEmail || value.isNotEmpty ){
+                        signInControl.isEmailValid(true);
+                      } else {
+                        signInControl.isEmailValid(false);
+                      }
+                    },
+                  ),
                 ),
               ),
-              onPressed: () {
-                debugPrint('login');
-              },
-              child: Text('Login'),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.always,
+                  child: TextFormField(
+                    clipBehavior: Clip.antiAlias,
+                    controller: senhaEditControl,
+                    style: const TextStyle(
+                      backgroundColor: Colors.white10,
+                    ),
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon( Icons.lock_open_outlined ),
+                      hintText: 'Digite sua senha',
+                      hintStyle: MyPassFonts.style.kLabelSmall(context, color: const Color.fromARGB(73, 0, 0, 0)),
+                      border: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0)
+                      ),
+                      fillColor: Colors.white54,
+                      filled: true,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: MyPassColors.purpleLight,
+                          width: 1.0
+                        ),
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      focusColor: MyPassColors.purpleLight,
+                    ),
+                    textInputAction: TextInputAction.done,
+                    validator: (value) => validInput.validationMessage( ValidationType.senha, value! ),
+                    onChanged: (value) {
+                      if (RegExp(r'^[a-zA-Z0-9_!@#.$]{6,20}$').hasMatch(value)){
+                        signInControl.isPassValid(true);
+                      } else {
+                        signInControl.isPassValid(false);
+                      }
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                child: Obx(() => 
+                  ElevatedButton(
+                    onPressed: (signInControl.isEmailValid.value && signInControl.isPassValid.value)
+                    ? () async {
+                      String email = emailEditControl.value.text.toString();
+                      String senha = senhaEditControl.value.text.toString();
+                      await signInControl.logIn(
+                        email,
+                        senha 
+                      );
+                    }
+                    : null,
+                    
+                    style: (signInControl.isEmailValid.value && signInControl.isPassValid.value)
+                    ? ElevatedButton.styleFrom(
+                        backgroundColor: MyPassColors.purpleLight,
+                        alignment: Alignment.center,
+                        fixedSize: Size(
+                          MediaQuery.of(context).size.width,
+                          56.0
+                        ),
+                        textStyle: const TextStyle(
+                          color: MyPassColors.whiteF0,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold
+                        )
+                    )
+                    : ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 137, 137, 137),
+                        alignment: Alignment.center,
+                        fixedSize: Size(
+                          MediaQuery.of(context).size.width,
+                          56.0
+                        ),
+                        textStyle: const TextStyle(
+                          color: MyPassColors.greyDarker,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold
+                        )
+                    ),
+                    child: Text('Login'),
+                  )
+                ),
+              ),
+            ],
+          ),
         ),
       )
     );
