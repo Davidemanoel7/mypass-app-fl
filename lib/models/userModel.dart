788bbe73d6bc8671c8ed7437706e126c53963f1a
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-09 17:44:37 -0300

    remove unused datas...

--- lib/models/userModel.dart
+++ lib/models/userModel.dart
@@ -2,38 +2,21 @@ class User{
   final String name;
   final String user;
   final String email;
-  final String password;
   final String? userType;
 
   User({
     required this.name,
     required this.user,
     required this.email,
-    required this.password,
     this.userType
   });
 
   factory User.fromJson( Map<String, dynamic> json) {
-    return switch ( json ) {
-      {
-        "message": String _,
-        "createdUser": {
-          "name": String name,
-          "user": String user,
-          "email": String email,
-          "password": String password,
-          "userType": String userType
-        },
-        "token": String _
-      } => User(
-        name: name,
-        user: user,
-        email: email,
-        password: password,
-        userType: userType
-      ),
-
-      _ => throw const FormatException('Failed to load json data')
-    };
+    return User(
+      name: json['name'] as String,
+      user: json['user'] as String,
+      email: json['email'] as String,
+      userType: json['userType'] ?? 'common'
+    );
   }
 }
