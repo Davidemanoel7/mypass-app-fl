64f4690127f601a6359871eb5f729c664af7d212
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-09 10:50:58 -0300

    Align content center

--- lib/screens/auth/signInView.dart
+++ lib/screens/auth/signInView.dart
@@ -17,162 +17,180 @@ class SignInView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox.expand(
-        child: Container(
-          margin: const EdgeInsets.all(24.0),
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24),
-                child: Image.asset(
-                  './lib/assets/images/myPass.png'  
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                child: Form(
-                  autovalidateMode: AutovalidateMode.always,
-                  child: TextFormField(
-                    controller: emailEditControl,
-                    style: const TextStyle(
-                      // backgroundColor: Colors.white10,
-                    ),
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon( Icons.email_outlined ),
-                      hintText: 'Digite seu email',
-                      hintStyle: MyPassFonts.style.kLabelSmall(context, color: const Color.fromARGB(73, 0, 0, 0)),
-                      border: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: MyPassColors.greyBD,
-                          strokeAlign: BorderSide.strokeAlignCenter,
-                        ),
-                        borderRadius: BorderRadius.circular(16.0)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: MyPassColors.purpleLight,
-                          width: 1.0
-                        ),
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                    ),
-                    textInputAction: TextInputAction.done,
-                    validator: (value) => validInput.validationMessage( ValidationType.email, value! ),
-                    onChanged: (value) {
-                      if ( value.isEmail ){
-                        signInControl.isEmailValid(true);
-                      } else {
-                        signInControl.isEmailValid(false);
-                      }
-                    },
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24),
+                  child: Image.asset(
+                    './lib/assets/images/myPass.png'  
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                child: Form(
-                  autovalidateMode: AutovalidateMode.always,
-                  child: TextFormField(
-                    clipBehavior: Clip.antiAlias,
-                    controller: senhaEditControl,
-                    style: const TextStyle(
-                      backgroundColor: Colors.white10,
-                    ),
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon( Icons.lock_open_outlined ),
-                      hintText: 'Digite sua senha',
-                      hintStyle: MyPassFonts.style.kLabelSmall(context, color: const Color.fromARGB(73, 0, 0, 0)),
-                      border: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: MyPassColors.greyBD,
-                          strokeAlign: BorderSide.strokeAlignCenter,
-                        ),
-                        borderRadius: BorderRadius.circular(16.0)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Form(
+                    autovalidateMode: AutovalidateMode.always,
+                    child: TextFormField(
+                      controller: emailEditControl,
+                      style: const TextStyle(
+                        // backgroundColor: Colors.white10,
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: MyPassColors.purpleLight,
-                          width: 1.0
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon( Icons.email_outlined ),
+                        hintText: 'Digite seu email',
+                        hintStyle: MyPassFonts.style.kLabelSmall(context, color: const Color.fromARGB(73, 0, 0, 0)),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: MyPassColors.greyBD,
+                            strokeAlign: BorderSide.strokeAlignCenter,
+                          ),
+                          borderRadius: BorderRadius.circular(16.0)
                         ),
-                        borderRadius: BorderRadius.circular(16.0),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: MyPassColors.purpleLight,
+                            width: 1.0
+                          ),
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
                       ),
+                      textInputAction: TextInputAction.done,
+                      validator: (value) => validInput.validationMessage( ValidationType.email, value! ),
+                      onChanged: (value) {
+                        if ( value.isEmail ){
+                          signInControl.isEmailValid(true);
+                        } else {
+                          signInControl.isEmailValid(false);
+                        }
+                      },
                     ),
-                    textInputAction: TextInputAction.done,
-                    validator: (value) => validInput.validationMessage( ValidationType.senha, value! ),
-                    onChanged: (value) {
-                      if (RegExp(r'^[a-zA-Z0-9_!@#.$]{6,20}$').hasMatch(value)){
-                        signInControl.isPassValid(true);
-                      } else {
-                        signInControl.isPassValid(false);
-                      }
-                    },
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24),
-                child: Obx(() =>
-                 signInControl.isEmailValid.value && signInControl.isPassValid.value ? 
-                  ElevatedButton(
-                    onPressed: () async {
-                      String email = emailEditControl.value.text;
-                      String senha = senhaEditControl.value.text;
-                      dynamic result = await signInControl.logIn( email, senha );
-                      if ( result['auth'] == true ) {
-                        Get.toNamed('/home');
-                      } else {
-                        debugPrint('Login falhou');
-                        // Get.snackbar("Oops", "Algo de errado aconteceu...");
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: MyPassColors.purpleLight,
-                      alignment: Alignment.center,
-                      fixedSize: Size(
-                        MediaQuery.of(context).size.width,
-                        56.0
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Form(
+                    autovalidateMode: AutovalidateMode.always,
+                    child: TextFormField(
+                      clipBehavior: Clip.antiAlias,
+                      controller: senhaEditControl,
+                      style: const TextStyle(
+                        backgroundColor: Colors.white10,
+                      ),
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon( Icons.lock_open_outlined ),
+                        hintText: 'Digite sua senha',
+                        hintStyle: MyPassFonts.style.kLabelSmall(context, color: const Color.fromARGB(73, 0, 0, 0)),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: MyPassColors.greyBD,
+                            strokeAlign: BorderSide.strokeAlignCenter,
+                          ),
+                          borderRadius: BorderRadius.circular(16.0)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: MyPassColors.purpleLight,
+                            width: 1.0
+                          ),
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      textInputAction: TextInputAction.done,
+                      obscureText: true,
+                      validator: (value) => validInput.validationMessage( ValidationType.senha, value! ),
+                      onChanged: (value) {
+                        if (RegExp(r'^[a-zA-Z0-9_!@#.$]{6,20}$').hasMatch(value)){
+                          signInControl.isPassValid(true);
+                        } else {
+                          signInControl.isPassValid(false);
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 8),
+                  child: GestureDetector(
+                    onTap: () => debugPrint('esqueci minha senha'),
+                    child: Text(
+                      'Esqueci minha senha',
+                      style: MyPassFonts.style.kLabelSmall(context,
+                        fontWeight: FontWeight.w500,
+                        color: MyPassColors.greyBD
                       ),
                     ),
-                    child: Obx(() => 
-                      !signInControl.authLoad.value ? 
-                      const Text(
-                        'Login',
-                        style: TextStyle(
-                          color: MyPassColors.whiteF0,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold
-                        )
-                      )
-                      :
-                      const CircularProgressIndicator(
-                        color: MyPassColors.whiteF0,
-                      )
-                    )
                   )
-                  : 
-                  ElevatedButton(
-                    onPressed: null,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: MyPassColors.greyBD,
-                      alignment: Alignment.center,
-                      fixedSize: Size(
-                        MediaQuery.of(context).size.width,
-                        56.0
-                      ),
-                    ),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(
-                        color: MyPassColors.greyDarker,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold
+                ),
+                Obx(() => 
+                  signInControl.authLoad.value ?
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24),
+                      child: CircularProgressIndicator(
+                        color: MyPassColors.purpleLight,
                       ),
                     )
-                  ) 
-                )
+                    :
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 24),
+                      child: Obx(() =>
+                        signInControl.isEmailValid.value && signInControl.isPassValid.value ? 
+                        ElevatedButton(
+                          onPressed: () async {
+                            String email = emailEditControl.value.text;
+                            String senha = senhaEditControl.value.text;
+                            dynamic result = await signInControl.logIn( email, senha );
+                            if ( result['auth'] == true ) {
+                              Get.toNamed('/home');
+                            } else {
+                              debugPrint('message: ${result['message']}');
+                              // Get.snackbar("Oops", "Algo de errado aconteceu...");
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: MyPassColors.purpleLight,
+                            alignment: Alignment.center,
+                            fixedSize: Size(
+                              MediaQuery.of(context).size.width,
+                              56.0
+                            ),
+                          ),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              color: MyPassColors.whiteF0,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold
+                            )
+                          )
+                        )
+                        :
+                        ElevatedButton(
+                          onPressed: null,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: MyPassColors.greyBD,
+                            alignment: Alignment.center,
+                            fixedSize: Size(
+                              MediaQuery.of(context).size.width,
+                              56.0
+                            ),
+                          ),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              color: MyPassColors.greyDarker,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold
+                            ),
+                          )
+                        ) 
+                      )
+                    ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -199,8 +217,10 @@ class SignInView extends StatelessWidget {
                     )
                   ],
                 )
-            ]),
+              ],
+            ),
           ),
+        ),
       ),
     );
   }
