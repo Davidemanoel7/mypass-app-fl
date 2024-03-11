2d1472e592c5e248deed3caf6d12c1d24df6d69a
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-11 17:49:30 -0300

    Add safe Area

--- lib/screens/home/homeView.dart
+++ lib/screens/home/homeView.dart
@@ -10,149 +10,151 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        title: Obx(
-          () => homeControll.userName.value != ''
-          ?
-            Text(
-              'Hi, ${homeControll.userName}',
-              style: MyPassFonts.style.kLabelMedium(context,
-                color: MyPassColors.black1B,
-                fontWeight: FontWeight.w700
-              ),
-            )
-          :
-            Text(
-              'Hi, ',
-              style: MyPassFonts.style.kLabelMedium(context,
-                color: MyPassColors.black1B,
-                fontWeight: FontWeight.w700
-              ),
-            ),
-        ),
-        centerTitle: true,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: IconButton(
-              onPressed: () => debugPrint('Ir para /account'),
-              icon: const Icon(
-                Icons.menu,
-                color: MyPassColors.purpleLight,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          title: Obx(
+            () => homeControll.userName.value != ''
+            ?
+              Text(
+                'Hi, ${homeControll.userName}',
+                style: MyPassFonts.style.kLabelMedium(context,
+                  color: MyPassColors.black1B,
+                  fontWeight: FontWeight.w700
+                ),
               )
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(24.0),
-        alignment: Alignment.center,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap:() => debugPrint('Criar senha'),
-                child: Container(
-                  height: 92,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 1,
-                      color: MyPassColors.whiteF0.withOpacity(0.2)
-                    ),
-                    borderRadius: BorderRadius.circular(24),
-                    gradient: const LinearGradient(
-                      colors: [
-                        MyPassColors.purpleLight,
-                        Color.fromARGB(255, 207, 20, 125),
-                      ],
-                      begin: AlignmentDirectional.topStart,
-                      end: AlignmentDirectional.bottomEnd
-                    )
-                  ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Text(
-                          'Nova senha',
-                          style: MyPassFonts.style.kTitleMedium(context,
-                            color: MyPassColors.whiteF0,
-                          ),
-                        ),
-                      ),
-                      const Icon(
-                        Icons.add_circle_outline,
-                        color: MyPassColors.whiteF0,
-                        size: 32,
-                      )
-                    ],
-                  ),
+            :
+              Text(
+                'Hi, ',
+                style: MyPassFonts.style.kLabelMedium(context,
+                  color: MyPassColors.black1B,
+                  fontWeight: FontWeight.w700
                 ),
               ),
-            ),
+          ),
+          centerTitle: true,
+          actions: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap:() => debugPrint('Minhas senhas'),
-                child: Container(
-                  height: 91,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 1,
-                      color: MyPassColors.whiteF0.withOpacity(0.2)
-                    ),
-                    borderRadius: BorderRadius.circular(24),
-                    gradient: const LinearGradient(
-                      colors: [
-                        MyPassColors.blueLight,
-                        MyPassColors.purpleLight
-                      ],
-                      begin: AlignmentDirectional.topStart,
-                      end: AlignmentDirectional.bottomEnd
-                    )
-                  ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Text(
-                          'Minhas senhas',
-                          style: MyPassFonts.style.kTitleMedium(context,
-                            color: MyPassColors.whiteF0,
-                          ),
-                        ),
-                      ),
-                      const Icon(
-                        Icons.password_rounded,
-                        color: MyPassColors.whiteF0,
-                        size: 32,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'v1.0.0',
-                    style: MyPassFonts.style.kLabelSmall(context,
-                      color: MyPassColors.greyBD 
-                    ),
-                  )
-                ],
+              padding: const EdgeInsets.only(right: 24),
+              child: IconButton(
+                onPressed: () => debugPrint('Ir para /account'),
+                icon: const Icon(
+                  Icons.menu,
+                  color: MyPassColors.purpleLight,
+                )
               ),
             )
           ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(24.0),
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap:() => debugPrint('Criar senha'),
+                  child: Container(
+                    height: 92,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: MyPassColors.whiteF0.withOpacity(0.2)
+                      ),
+                      borderRadius: BorderRadius.circular(24),
+                      gradient: const LinearGradient(
+                        colors: [
+                          MyPassColors.purpleLight,
+                          Color.fromARGB(255, 207, 20, 125),
+                        ],
+                        begin: AlignmentDirectional.topStart,
+                        end: AlignmentDirectional.bottomEnd
+                      )
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Text(
+                            'Nova senha',
+                            style: MyPassFonts.style.kTitleMedium(context,
+                              color: MyPassColors.whiteF0,
+                            ),
+                          ),
+                        ),
+                        const Icon(
+                          Icons.add_circle_outline,
+                          color: MyPassColors.whiteF0,
+                          size: 32,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap:() => debugPrint('Minhas senhas'),
+                  child: Container(
+                    height: 91,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: MyPassColors.whiteF0.withOpacity(0.2)
+                      ),
+                      borderRadius: BorderRadius.circular(24),
+                      gradient: const LinearGradient(
+                        colors: [
+                          MyPassColors.blueLight,
+                          MyPassColors.purpleLight
+                        ],
+                        begin: AlignmentDirectional.topStart,
+                        end: AlignmentDirectional.bottomEnd
+                      )
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Text(
+                            'Minhas senhas',
+                            style: MyPassFonts.style.kTitleMedium(context,
+                              color: MyPassColors.whiteF0,
+                            ),
+                          ),
+                        ),
+                        const Icon(
+                          Icons.password_rounded,
+                          color: MyPassColors.whiteF0,
+                          size: 32,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'v1.0.0',
+                      style: MyPassFonts.style.kLabelSmall(context,
+                        color: MyPassColors.greyBD 
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
