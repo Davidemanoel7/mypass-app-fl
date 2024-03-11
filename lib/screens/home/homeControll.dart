64d018ef3406f71cb1851376a3e48d29760f3428
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-11 17:15:07 -0300

    Init method to get username on cache

--- lib/screens/home/homeControll.dart
+++ lib/screens/home/homeControll.dart
@@ -4,12 +4,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeControll extends GetxController {
   
+  var userName = ''.obs;
+
+  @override
+  void onInit() async {
+    final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    final String? user = sharedPreferences.getString('user');
+    userName(user);
+    super.onInit();
+  }
 
   Future<bool> logOut () async {
     try {
       final SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
       await sharedPrefs.setBool('authenticated', false);
       await sharedPrefs.setString('token', '');
+      await sharedPrefs.setString('user', '');
       return true;
     } catch (e) {
       debugPrint('\nError: $e');
