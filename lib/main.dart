cf863ce27011c1f5962a70e0133acfd025581668
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-12 21:23:33 -0300

    include dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import './controllers/initializerApp/AppStarter.dart';
 
-void main() {
-
+void main() async {
+  await dotenv.load(fileName: ".env");
   // force portrait position
   FlutterNativeSplash.preserve(widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
   SystemChrome.setPreferredOrientations([
