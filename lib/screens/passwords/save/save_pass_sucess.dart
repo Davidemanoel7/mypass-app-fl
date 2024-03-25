e9cd269f884c92b323feb396ab4fd090722badf4
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-25 12:01:49 -0300

    Success page

---
+++ lib/screens/passwords/save/save_pass_sucess.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SaveSuccessView extends StatelessWidget {
+  const SaveSuccessView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
