e6a570b018a7938b8da23f5160f57ab1cafc36e4
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-28 17:24:28 -0300

    Store datas on cache

--- lib/screens/auth/signInControll.dart
+++ lib/screens/auth/signInControll.dart
@@ -3,27 +3,28 @@ import 'dart:convert';
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mypass/managers/cache_manager.dart';
 import 'package:mypass/models/tokenJWT.dart';
 import 'package:mypass/services/fetchData.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SignInControl extends GetxController{
+class SignInControl extends GetxController with SharedPrefManager {
   
   var isAuth = false.obs;
   var isEmailValid = false.obs;
   var isPassValid = false.obs;
-  var token = ''.obs;
+  // var token = ''.obs;
 
-  var email = ''.obs;
-  var user = ''.obs;
-  var userId = ''.obs;
-  var userType = ''.obs;
+  // var email = ''.obs;
+  // var user = ''.obs;
+  // var userId = ''.obs;
+  // var userType = ''.obs;
 
   var authLoad = false.obs;
 
   Future<dynamic> logIn( String email, String senha) async {
     try {
-      final SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
+      // final SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
 
       authLoad(true);
       final dynamic body = { 'email': email, 'password': senha };
@@ -31,33 +32,25 @@ class SignInControl extends GetxController{
         Requests.signIn,
         body: body
       );
-      
+      authLoad(false);
+
       var respbody = jsonDecode(resp.body) as Map<String, dynamic>;
 
       switch ( resp.statusCode ) {
         case 200:
           var auth = Auth.fromJson( respbody );
-          final jwt = JWT.decode(auth.token!);
-          this.email = RxString(email);
 
-          // store userId on cache
-          userId = RxString(jwt.payload['userId']);
-          await sharedPrefs.setString('userId', jwt.payload['userId']);
+          final acessJwt = JWT.decode(auth.acessToken!);
 
-          user = RxString(jwt.payload['user']);
-          await sharedPrefs.setString( 'user', jwt.payload['user'] );
+          await setItemFromCache('acessToken', TypeKey.String, auth.acessToken );
+          await setItemFromCache('refreshToken', TypeKey.String, auth.refreshToken );
 
-          userType = RxString(jwt.payload['userType']);
+          await setItemFromCache('isAuth', TypeKey.bool, true );
 
-          await sharedPrefs.setString('email', email);
+          await setItemFromCache('userIdKey', TypeKey.String, acessJwt.payload['userId']);
 
-          // store token on cache
-          token = RxString(jwt.toString());
-          await sharedPrefs.setString( 'token', auth.token! );
-
-          // store auth on cache
-          isAuth( await sharedPrefs.setBool('authenticated', true) );
-          authLoad(false);
+          var userID = await getItemFromCache('userIdKey', TypeKey.String);
+          debugPrint('$userID');
 
           dynamic response = {
             'auth': true,
@@ -69,7 +62,6 @@ class SignInControl extends GetxController{
             'auth': false,
             'message': 'Senha incorreta'
           };
-          authLoad(false);
           return response;
 
         case 404:
@@ -77,7 +69,6 @@ class SignInControl extends GetxController{
             'auth': false,
             'message': 'Nenhum usuário encontrado com este email.'
           };
-          authLoad(false);
           return response;
 
         default:
@@ -85,12 +76,10 @@ class SignInControl extends GetxController{
             'auth': false,
             'message': 'Oops. talvez estejamos com instabilidade no servidor. Tente novamentte mais tarde'
           };
-          authLoad(false);
           return response;
       }
     } catch (e) {
       debugPrint('\nError: $e');
-      authLoad(false);
       return false; 
     }
   }
