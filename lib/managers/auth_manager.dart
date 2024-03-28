b8f660ba5baae5825eebc53314353c8a5daa9f5b
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-28 15:48:58 -0300

    Init Auth and Cache manager

---
+++ lib/managers/auth_manager.dart
@@ -0,0 +1,56 @@
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
+import 'package:mypass/managers/cache_manager.dart';
+
+class AuthenticationManager extends GetxController with SharedPrefManager{
+  final isLogged = false.obs;
+
+  
+  @override
+  void onInit() {
+    debugPrint('\t# Check login Status');
+    checkLoginStatus();
+    super.onInit();
+  }
+  
+  void logout() {
+    isLogged.value = false;
+    removeToken();
+  }
+
+  void login(String? token) async {
+    isLogged.value = true;
+    await saveToken(token!);
+  }
+
+  void checkLoginStatus() async {
+    bool isAuth = await checkJwt();
+    if ( isAuth ) {
+      isLogged.value = true;
+    } else {
+      isLogged.value = false;
+    }
+  }
+
+  Future<bool> checkJwt() async {
+    String? jwtKey = dotenv.env['JWT_KEY'];
+    String? token = await getToken();
+    try {
+      if (token == null ) {
+        return false;
+      }
+
+      final jwt = JWT.verify(token, SecretKey(jwtKey!));
+      debugPrint('Paylodad: ${jwt.payload}');
+      return true;
+    } on JWTExpiredException {
+      debugPrint('JWT Expired. Fetch data to refresh token...');
+      return false;
+    } on JWTException catch ( e ) {
+      debugPrint(e.message);
+      return false;
+    }
+  }
+}
---
+++ lib/managers/cache_manager.dart
@@ -0,0 +1,82 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+mixin SharedPrefManager {
+
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<void> saveToken(String token) async {
+    await _prefs.then(( p )=> p.setString( CacheKey.TOKEN.toString(), token ));
+  }
+
+  Future<String?> getToken() async {
+    return await _prefs.then(( p ) => p.getString( CacheKey.TOKEN.toString() ));
+  }
+
+  Future<void> removeToken() async {
+    await _prefs.then((p) => p.remove( CacheKey.TOKEN.toString() ));
+  }
+
+  Future<void> setOnboard() async {
+    await _prefs.then(( p ) => p.setBool( CacheKey.ONBOARD.toString(), true ));
+  }
+
+  Future<bool?> getOnboard() async {
+    return await _prefs.then((p) => p.getBool(CacheKey.ONBOARD.toString()) );
+  }
+
+  Future<void> removeOnboard() async {
+    await _prefs.then((p) => p.remove( CacheKey.ONBOARD.toString() ));
+  }
+
+  Future<dynamic> getItemFromCache( String key, TypeKey type ) async {
+    switch ( type ) {
+      case TypeKey.bool:
+        return await _prefs.then((p) => p.getBool(key));
+      case TypeKey.String:
+        return await _prefs.then((p) => p.getString(key));
+      case TypeKey.Int:
+        return await _prefs.then((p) => p.getInt(key));
+      case TypeKey.double:
+        return await _prefs.then((p) => p.getDouble(key));
+      case TypeKey.dynamic:
+        return await _prefs.then((p) => p.getStringList(key));
+      default:
+        return null;
+    }
+  }
+
+  Future<void> setItemFromCache( String key, TypeKey type, dynamic value ) async {
+    switch ( type ) {
+      case TypeKey.bool:
+        await _prefs.then((p) => p.setBool(key, value));
+        break;
+      case TypeKey.String:
+        await _prefs.then((p) => p.setString(key, value));
+        break;
+      case TypeKey.Int:
+        await _prefs.then((p) => p.setInt(key, value));
+        break;
+      case TypeKey.double:
+        await _prefs.then((p) => p.setDouble(key, value));
+        break;
+      case TypeKey.dynamic:
+        await _prefs.then((p) => p.setStringList(key, value));
+        break;
+      default:
+        return;
+    }
+  }
+}
+
+enum CacheKey {
+  TOKEN,
+  ONBOARD
+}
+
+enum TypeKey {
+  bool,
+  String,
+  Int,
+  dynamic,
+  double,
+}
