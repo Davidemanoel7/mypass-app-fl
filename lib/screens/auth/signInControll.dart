8b7e47d4d95fd1c1a88dd7051b70d5140c318dab
Davidemanoel7 <davidemanoel34@gmail.com>
2024-03-29 15:43:21 -0300

    update controller using auth Manager response

--- lib/screens/auth/signInControll.dart
+++ lib/screens/auth/signInControll.dart
@@ -3,29 +3,24 @@ import 'dart:convert';
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mypass/managers/auth_manager.dart';
 import 'package:mypass/managers/cache_manager.dart';
 import 'package:mypass/models/tokenJWT.dart';
 import 'package:mypass/services/fetchData.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SignInControl extends GetxController with SharedPrefManager {
+
+  AuthenticationManager authManager = Get.find<AuthenticationManager>();
   
   var isAuth = false.obs;
   var isEmailValid = false.obs;
   var isPassValid = false.obs;
-  // var token = ''.obs;
-
-  // var email = ''.obs;
-  // var user = ''.obs;
-  // var userId = ''.obs;
-  // var userType = ''.obs;
 
   var authLoad = false.obs;
 
   Future<dynamic> logIn( String email, String senha) async {
     try {
-      // final SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
-
       authLoad(true);
       final dynamic body = { 'email': email, 'password': senha };
       var resp = await fetchData(
@@ -40,20 +35,10 @@ class SignInControl extends GetxController with SharedPrefManager {
         case 200:
           var auth = Auth.fromJson( respbody );
 
-          final acessJwt = JWT.decode(auth.acessToken!);
-
-          await setItemFromCache('acessToken', TypeKey.String, auth.acessToken );
-          await setItemFromCache('refreshToken', TypeKey.String, auth.refreshToken );
-
-          await setItemFromCache('isAuth', TypeKey.bool, true );
-
-          await setItemFromCache('userIdKey', TypeKey.String, acessJwt.payload['userId']);
-
-          var userID = await getItemFromCache('userIdKey', TypeKey.String);
-          debugPrint('$userID');
+          bool logged = await authManager.login( auth.acessToken!, auth.refreshToken! );
 
           dynamic response = {
-            'auth': true,
+            'auth': logged,
           };
           return response;
 
