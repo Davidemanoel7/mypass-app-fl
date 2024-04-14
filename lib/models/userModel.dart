87033503198180f23193974496534911b68e01e8
Davidemanoel7 <davidemanoel34@gmail.com>
2024-04-14 15:27:13 -0300

    update model

--- lib/models/userModel.dart
+++ lib/models/userModel.dart
@@ -1,14 +1,16 @@
 class User{
-  final String name;
-  final String user;
-  final String email;
+  String name;
+  String user;
+  String email;
   final String? userType;
+  String? profileImage;
 
   User({
     required this.name,
     required this.user,
     required this.email,
-    this.userType
+    this.userType,
+    this.profileImage
   });
 
   factory User.fromJson( Map<String, dynamic> json) {
@@ -16,7 +18,20 @@ class User{
       name: json['name'] as String,
       user: json['user'] as String,
       email: json['email'] as String,
-      userType: json['userType'] ?? 'common'
+      userType: json['userType'] ?? 'common',
+      profileImage: json['profileImage']
     );
   }
+
+  String getName () => name;
+  void setName( String newName ) => name = newName;
+
+  String getUser () => user;
+  void setUser( String newUsr ) => user = newUsr;
+
+  String getEmail () => email;
+  void setEmail( String newEmail ) => email = newEmail;
+
+  String getProfileImage () => profileImage!;
+  void setProfileImage( String newProfileImage ) => profileImage = newProfileImage;
 }
