96421c0606bc61dd3db7224931daf98232fecb81
Davidemanoel7 <davidemanoel34@gmail.com>
2024-04-18 12:12:16 -0300

    Use shimmer effect

--- lib/screens/home/homeView.dart
+++ lib/screens/home/homeView.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mypass/screens/home/homeControll.dart';
 import 'package:mypass/utils/themes.dart';
+import 'package:mypass/widgets/components/customShimmer.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({super.key});
@@ -16,28 +18,48 @@ class HomeView extends StatelessWidget {
           backgroundColor: Colors.transparent,
           elevation: 0,
           automaticallyImplyLeading: false,
-          title: Obx(() => 
-            Text(
-              'Olá, ${homeControll.loadRequest.value ? '' : homeControll.usr.value.user}',
-              style: MyPassFonts.style.kLabelMedium(context,
-                color: MyPassColors.black1B,
-                fontWeight: FontWeight.w700
-              ),
-            )
+          title: Obx(() => homeControll.loadRequest.value
+            ? const CustomShimmer(
+                height: 8,
+                width: 128, 
+                borderRadius: 8
+              )
+            : Text(
+                'Olá, ${homeControll.usr.value.user}',
+                style: MyPassFonts.style.kLabelMedium(context,
+                  color: MyPassColors.black1B,
+                  fontWeight: FontWeight.w700
+                ),
+              )
           ),
           centerTitle: true,
           actions: [
             Padding(
               padding: const EdgeInsets.only(right: 24),
-              child: IconButton(
-                onPressed: () {
-                  Get.toNamed('/profile');
-                },
-                icon: const Icon(
-                  Icons.menu,
-                  color: MyPassColors.purpleLight,
+              child:
+                Obx(() =>
+                  homeControll.loadRequest.value
+                  ? SizedBox(
+                      width: 24,
+                      height: 24,
+                      child: Shimmer.fromColors(
+                        baseColor: MyPassColors.greyBD,
+                        highlightColor: MyPassColors.whiteF0,
+                        child: const Icon(
+                          Icons.menu
+                        ),
+                      ),
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        Get.toNamed('/profile');
+                      },
+                      icon: const Icon(
+                        Icons.menu,
+                        color: MyPassColors.purpleLight,
+                      )
+                    ),
                 )
-              ),
             )
           ],
         ),
@@ -49,45 +71,53 @@ class HomeView extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: GestureDetector(
-                  onTap:() => Get.toNamed('/create/password'),
-                  child: Container(
-                    height: 92,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 1,
-                        color: MyPassColors.whiteF0.withOpacity(0.2)
-                      ),
-                      borderRadius: BorderRadius.circular(24),
-                      gradient: const LinearGradient(
-                        colors: [
-                          MyPassColors.purpleLight,
-                          Color.fromARGB(255, 207, 20, 125),
-                        ],
-                        begin: AlignmentDirectional.topStart,
-                        end: AlignmentDirectional.bottomEnd
-                      )
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
-                            'Nova senha',
-                            style: MyPassFonts.style.kTitleMedium(context,
-                              color: MyPassColors.whiteF0,
-                            ),
+                child: Obx(() =>
+                  GestureDetector(
+                    onTap:() => homeControll.loadRequest.value ? null : Get.toNamed('/create/password'),
+                    child: homeControll.loadRequest.value
+                    ? CustomShimmer(
+                      height: 92,
+                      width: MediaQuery.of(context).size.width,
+                      borderRadius: 24.0
+                    )
+                    : Container(
+                        height: 92,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 1,
+                            color: MyPassColors.whiteF0.withOpacity(0.2)
                           ),
+                          borderRadius: BorderRadius.circular(24),
+                          gradient: const LinearGradient(
+                            colors: [
+                              MyPassColors.purpleLight,
+                              Color.fromARGB(255, 207, 20, 125),
+                            ],
+                            begin: AlignmentDirectional.topStart,
+                            end: AlignmentDirectional.bottomEnd
+                          )
                         ),
-                        const Icon(
-                          Icons.add_circle_outline,
-                          color: MyPassColors.whiteF0,
-                          size: 32,
-                        )
-                      ],
-                    ),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              child: Text(
+                                'Nova senha',
+                                style: MyPassFonts.style.kTitleMedium(context,
+                                  color: MyPassColors.whiteF0,
+                                ),
+                              ),
+                            ),
+                            const Icon(
+                              Icons.add_circle_outline,
+                              color: MyPassColors.whiteF0,
+                              size: 32,
+                            )
+                          ],
+                        ),
+                      ),
                   ),
-                ),
+                )
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
