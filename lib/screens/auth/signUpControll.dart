509ce227f4b3f4d4db9607871599729b3a732624
Davidemanoel7 <davidemanoel34@gmail.com>
2024-04-20 15:51:11 -0300

    refactored

--- lib/screens/auth/signUpControll.dart
+++ lib/screens/auth/signUpControll.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mypass/managers/auth_manager.dart';
 import 'package:mypass/models/userModel.dart';
 import 'package:mypass/services/fetchData.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -15,6 +16,8 @@ class SignUpControl extends GetxController{
 
   var signUpLoad = false.obs;
 
+  AuthenticationManager authenticationManager = Get.put(AuthenticationManager());
+
   Future<dynamic> signUp( String name, String user, String email, String senha ) async {
     dynamic body = {
       'name': name,
@@ -25,34 +28,30 @@ class SignUpControl extends GetxController{
 
     try {
       signUpLoad(true);
-      final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-
       var resp = await fetchData(
         Requests.signUp,
         body: body
       );
+      signUpLoad(false);
+
       switch ( resp.statusCode ) {
         case 201:
           var responseBody = jsonDecode(resp.body) as Map<String, dynamic>;
           User.fromJson( responseBody['createdUser'] as Map<String, dynamic>);
-          JWT.decode( responseBody['token']);
-          await sharedPreferences.setString('token', responseBody['token']);
+          await authenticationManager.saveToken('acessToken', responseBody['acessToken']);
+          await authenticationManager.saveToken('refreshToken', responseBody['refreshToken']);
           
-          signUpLoad(false);
           return {
             "created": true,
           };
 
         case 409:
-          signUpLoad(false);
           return {
             "created": false,
             "message": 'Este email já foi usado por outro usuário. Tente outro!'
           };
 
         default:
-          jsonDecode(resp.body) as Map<String, dynamic>;
-          signUpLoad(false);
           return {
             "created": false,
             "message": 'Não foi possível criar a conta. Tente novamente...'
@@ -60,8 +59,6 @@ class SignUpControl extends GetxController{
       }
 
     } catch (e) {
-
-      signUpLoad(false);
       debugPrint('$e');
       return {
         "create": false,
