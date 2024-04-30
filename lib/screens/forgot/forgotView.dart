102c11b4d077d39c06393172400200e39c55c9ec
Davidemanoel7 <davidemanoel34@gmail.com>
2024-04-30 13:41:04 -0300

    BottomSheet

--- lib/screens/forgot/forgotView.dart
+++ lib/screens/forgot/forgotView.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mypass/screens/forgot/forgotControll.dart';
@@ -13,6 +11,7 @@ class ForgorView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    RxBool send = false.obs;
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -101,153 +100,129 @@ class ForgorView extends StatelessWidget {
                         color: MyPassColors.purpleLight,
                       )
                       :
-                      Obx(() => forgotControll.emailValid.value ?
-                        ElevatedButton(
-                          onPressed: () async {
-                            String email = emailEditControl.value.text;
-                            Map<String, dynamic> result = await forgotControll.forgot( email );
-
-                            if ( result['sended'] == true ){
-                              Get.dialog(
-                                Center(
-                                  child: BackdropFilter(
-                                    filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
-                                    child: Container(
-                                      width: MediaQuery.of(context).size.width * 0.65,
-                                      height: MediaQuery.of(context).size.height * 0.25,
-                                      padding: const EdgeInsets.all(16),
-                                      decoration: BoxDecoration(
-                                        gradient: LinearGradient(
-                                          colors: [
-                                            MyPassColors.whiteF0,
-                                            MyPassColors.whiteF0.withOpacity(0.9),
-                                          ],
-                                          begin: AlignmentDirectional.topStart,
-                                          end: AlignmentDirectional.bottomEnd,
-                                        ),
-                                        borderRadius: const BorderRadius.all(Radius.circular(16)),
-                                        border: Border.all(
-                                          width: 1,
-                                          color: MyPassColors.blueLight.withOpacity(0.2)
-                                        )
-                                      ),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.center,
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            'Email enviado!',
-                                            style: MyPassFonts.style.kTitleMedium(context,
-                                              color: MyPassColors.purpleLight,
-                                              fontWeight: FontWeight.bold
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.only(top: 8, bottom: 16),
-                                            child: Text(
-                                              'Link de recuperação enviado para seu email. Verifique sua caixa de entrada. [Expira em 1 hora]',
-                                              style: MyPassFonts.style.kLabelMedium(context,
-                                                color: MyPassColors.black1B,
-                                              ),
-                                              textAlign: TextAlign.justify,
-                                            ),
-                                          ),
-                                          Row(
-                                            crossAxisAlignment: CrossAxisAlignment.end,
-                                            mainAxisAlignment: MainAxisAlignment.end,
-                                            children: [
-                                              GestureDetector(
-                                                onTap: () => Get.back(),
-                                                child: Text(
-                                                  'Ok',
-                                                  style: MyPassFonts.style.kTitleLarge(context,
-                                                    color: MyPassColors.purpleLight,
-                                                    fontSize: 16
-                                                  ),
-                                                  textAlign: TextAlign.center,
-                                                ),
-                                              ),
-                                            ],
-                                          )
-                                        ],
-                                      ),     
+                      ElevatedButton(
+                        onPressed: forgotControll.emailValid.value ?
+                        () async {
+                          String email = emailEditControl.value.text;
+                          Map<String, dynamic> result = await forgotControll.forgot( email );
+                          send(true);
+                          if ( result['sended'] == true ){
+                            Get.bottomSheet(
+                              BottomSheet(
+                                onClosing: (){},
+                                builder: (_) =>
+                                  Container(
+                                    width: context.width,
+                                    height: context.height * 0.3,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.rectangle,
+                                      borderRadius: BorderRadius.all( Radius.circular(32) ),
+                                      color: MyPassColors.whiteF0,
                                     ),
-                                  ),
+                                    padding: const EdgeInsets.all(24),
+                                    child: Column(
+                                      children: [
+                                        const Text(
+                                          'Enviado!',
+                                          style: TextStyle(
+                                            color: MyPassColors.purpleLight,
+                                            fontSize: 20
+                                          ),
+                                        ),
+                                        const Padding(
+                                          padding: EdgeInsets.symmetric(vertical: 8.0),
+                                          child: Text(
+                                            'Enviamos um link de recuperação para seu email. Verifique sua caixa de entrada e altere sua senha. [O link expira em 1 hora]',
+                                            style: TextStyle(
+                                              color: MyPassColors.black1B,
+                                              fontSize: 16
+                                            ),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(top: 16.0),
+                                          child: ElevatedButton(
+                                            onPressed: () {
+                                              Get.back();
+                                            },
+                                            style: ElevatedButton.styleFrom(
+                                              fixedSize: Size(
+                                                context.width - 48,
+                                                64
+                                              ),
+                                              backgroundColor: MyPassColors.purpleLight
+                                            ),
+                                            child: Text(
+                                              'Ok',
+                                              style: MyPassFonts.style.kLabelLarge(
+                                                context,
+                                                color: MyPassColors.whiteF0,
+                                              ),
+                                            )
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                              )
+                            );
+                          } else {
+                            Get.snackbar(
+                              "Oops",
+                              "",
+                              titleText: Text(
+                                'Oops',
+                                style: MyPassFonts.style.kLabelLarge(context,
+                                  fontWeight: FontWeight.bold,
+                                  color: MyPassColors.purpleLight,
                                 ),
-                              );
-                            } else {
-                              Get.snackbar(
-                                "Oops",
-                                "",
-                                titleText: Text(
-                                  'Oops',
-                                  style: MyPassFonts.style.kLabelLarge(context,
-                                    fontWeight: FontWeight.bold,
-                                    color: MyPassColors.purpleLight,
-                                  ),
+                              ),
+                              messageText: Text(
+                                '${result['message']}',
+                                style: MyPassFonts.style.kLabelSmall(context,
+                                  fontWeight: FontWeight.w400,
+                                  color: MyPassColors.black1B
                                 ),
-                                messageText: Text(
-                                  '${result['message']}',
-                                  style: MyPassFonts.style.kLabelSmall(context,
-                                    fontWeight: FontWeight.w400,
-                                    color: MyPassColors.black1B
-                                  ),
-                                ),
-                                colorText: MyPassColors.black1B,
-                                leftBarIndicatorColor: MyPassColors.redAlert,
-                                animationDuration: const Duration( seconds: 1 ),
-                                backgroundColor: Colors.white,
-                                snackPosition: SnackPosition.TOP,
-                                borderWidth: 2,
-                                borderRadius: 16,
-                                borderColor: MyPassColors.whiteF0,
-                                isDismissible: true,
-                                dismissDirection: DismissDirection.horizontal,
-                                duration: const Duration( seconds: 4 ),
-                                forwardAnimationCurve: Curves.easeInOutQuad,
-                              );
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: MyPassColors.purpleLight,
-                            alignment: Alignment.center,
-                            fixedSize: Size(
-                              MediaQuery.of(context).size.width,
-                              56.0
-                            ),
+                              ),
+                              colorText: MyPassColors.black1B,
+                              leftBarIndicatorColor: MyPassColors.redAlert,
+                              animationDuration: const Duration( seconds: 1 ),
+                              backgroundColor: Colors.white,
+                              snackPosition: SnackPosition.TOP,
+                              borderWidth: 2,
+                              borderRadius: 16,
+                              borderColor: MyPassColors.whiteF0,
+                              isDismissible: true,
+                              dismissDirection: DismissDirection.horizontal,
+                              duration: const Duration( seconds: 4 ),
+                              forwardAnimationCurve: Curves.easeInOutQuad,
+                            );
+                          }
+                        }
+                        : null,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: forgotControll.emailValid.value
+                          ?MyPassColors.purpleLight
+                          : MyPassColors.greyBD,
+                          alignment: Alignment.center,
+                          fixedSize: Size(
+                            MediaQuery.of(context).size.width,
+                            56.0
                           ),
-                          child: const Text(
-                            'Enviar',
-                            style: TextStyle(
-                              color: MyPassColors.whiteF0,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold
-                            ),
-                          )
-                        )
-                        :
-                        ElevatedButton(
-                          onPressed: null,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: MyPassColors.greyBD,
-                            alignment: Alignment.center,
-                            fixedSize: Size(
-                              MediaQuery.of(context).size.width,
-                              56.0
-                            ),
+                        ),
+                        child: Text(
+                          'Enviar',
+                          style: TextStyle(
+                            color: forgotControll.emailValid.value
+                            ? MyPassColors.whiteF0
+                            : MyPassColors.greyDarker,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold
                           ),
-                          child: const Text(
-                            'Enviar',
-                            style: TextStyle(
-                              color: MyPassColors.greyDarker,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold
-                            ),
-                          )
                         )
                       )
                     )
-                )
+                ),
               ],
             ),
           ),
